11545acf0c876900d08216c6835f979c220846e8
prathamesh <prathameshkalburgi@gmail.com>
2023-07-13 02:04:16 +0530

    delete procedural program

--- src/main/java/org/problems/Conference/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-//package org.problems.Conference;
-//
-//public class InvalidInputException extends Exception{
-//    public InvalidInputException(String message) {
-//        super(message);
-//    }
-//}
